d6f6500bf348216df458b9cf693a81baee01391e
carolina_tapia <cstapiarubio@gmail.com>
2020-04-17 18:59:27 -0400

    mapa listo

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
     console.log("pagina cargada");
 });
+
+/*mapa sucursales*/ 
+var map;
+function initMap(){
+    map= new google.maps.Map(document.getElementById('map'), {
+        center:{lat:-33.4316452,lng:-70.6457336},
+        zoom:13
+    });
+}
     
