c99c9b131ad870e43e8cd0712f3be6270c895145
carolina_tapia <cstapiarubio@gmail.com>
2020-04-19 15:46:21 -0400

    modificando formularios

--- src/js/sucursal.js
+++ src/js/sucursal.js
@@ -3,6 +3,11 @@ var map;
 function initMap(){
     map= new google.maps.Map(document.getElementById('map'), {
         center:{lat:-33.4316452,lng:-70.6457336},
-        zoom:13
+        zoom:18
     });
+
+    var marker = new google.maps.Marker({
+        position: {lat:-33.4316452,lng:-70.6457336},
+        map : map
+    })
 }
