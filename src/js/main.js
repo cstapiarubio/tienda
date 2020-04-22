ca3adc7234e61ceccbf8bea06fde1548f8bf4c88
carolina_tapia <cstapiarubio@gmail.com>
2020-04-21 20:46:32 -0400

    listo navbar fijo y redimensionado de logo al hacer scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
     console.log("pagina cargada");
 });
+
+$(function() {
+    // esta parte es la que controla cuando se mueve el scroll y ejecuta una función
+    $(document).scroll(function() {
+      // aca se pregunta si el scroll se movio de para abajo.
+      if ($(this).scrollTop() > 1) { 
+        $('#logo img').css('width', '20%')
+      }
+      if ($(this).scrollTop() < 1) {
+        $('#logo img').css('width', '40%');
+      }
+    });
+  });
     
