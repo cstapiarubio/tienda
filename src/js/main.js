71b3def27b026311976ff95b7fd0b3f11eeb44b1
Carlos Arellano <arelacarliz@gmail.com>
2020-04-22 22:55:55 -0400

    css y maquetacion de 'login.html' listo

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
     console.log("pagina cargada");
+    
+    $(function() {
+      // esta parte es la que controla cuando se mueve el scroll y ejecuta una función
+      $(document).scroll(function() {
+        // aca se pregunta si el scroll se movio para abajo.
+        if ($(this).scrollTop() > 1) { 
+          $('#logo img').css('width', '20%');
+          $('#contact-div').hide();
+        }
+        if ($(this).scrollTop() < 1) {
+          $('#logo img').css('width', '40%');
+          $('#contact-div').show();
+        }
+      });
+    });
 });
 
-$(function() {
-    // esta parte es la que controla cuando se mueve el scroll y ejecuta una función
-    $(document).scroll(function() {
-      // aca se pregunta si el scroll se movio para abajo.
-      if ($(this).scrollTop() > 1) { 
-        $('#logo img').css('width', '20%');
-        $('#contact-div').hide();
-      }
-      if ($(this).scrollTop() < 1) {
-        $('#logo img').css('width', '40%');
-        $('#contact-div').show();
-      }
-    });
-  });
     
