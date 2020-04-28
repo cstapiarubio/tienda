da7f406d68ae50ffb6c9ccb8b3d273f490f1d034
carolina_tapia <cstapiarubio@gmail.com>
2020-04-27 23:58:09 -0400

    listo boton remove subcategoria

--- src/js/form.js
+++ src/js/form.js
@@ -14,29 +14,30 @@ $(document).ready(function() {
       return false;
     });
 
-    
-      $(".remove").click(function(){
-        console.log("hola");
-        $(this).remove();
-      });
-
-
-  });
-
-  $(function(){
-    $('#add_sub').on('click',function(){
-        var r= $('<div class="sub"><label for="inputNamesub" class="trash">Nombre subcategoría</label>' +
-        '<input type="text" class="form-control trash" id="inputNamesub" placeholder="Nombre de subcategoría"></input>'+ '<i class="far fa-times-circle remove trash"></i></div>');
-        $("#sub").append(r);
-    });
-
-
     $('.new').on('click',function(){
       console.log("hola");
-      $("#name_category").toggleClass("show");
+      $("#name_category").remove(".hide");
     });
 
-});
+
+    $('#add_sub').on('click',function(){
+      var r= $('<div class="sub"><label for="inputNamesub" class="trash">Nombre subcategoría</label>' +
+      '<input type="text" class="form-control trash" id="inputNamesub" placeholder="Nombre de subcategoría"></input>'+ '<i class="far fa-times-circle remove trash"></i></div>');
+      $("#sub").append(r);
+  });
+
+    
+    $(document).on('click', '.sub',function(){
+      $(this).remove();
+    });
+      
+  });
+
+
+
+    
+
+
 
 
 
